7a9c28145516f0f1e1c954d5bc53248ef42e985d
Rithika <19cs076rithikak@ksrce.ac.in>
2023-05-19 15:36:00 +0530

    HomePage  updated

--- src/Components/HomePage/Explore/Explore.jsx
+++ src/Components/HomePage/Explore/Explore.jsx
@@ -4,23 +4,81 @@ import Accordion from 'react-bootstrap/Accordion';
 
 function Explore() {
     const explores=[{
-        name:""
-    }]
+        name:"Popular cuisines near me",
+        list:[
+            "BBQ food near me",
+            "Bakery food near me",
+            "Beverages food near me",
+            "Biryani food near me",
+            "Burger food near me",
+            "Chettinad food near me",
+            "Chinese food near me",
+            "Desserts food near me",
+            "Ice Cream food near me",
+            "Juices food near me",
+            "Lebanese food near me",
+            " Mughlai food near me",
+            "North Indian food near me",
+            "Pizza food near me",
+            "Sandwich food near me",
+            "Seafood food near me",
+            "Shake food near me",
+            "Sichuan food near me",
+            "South Indian food near me",
+            "Street food near me"
+        ]
+    },{
+        name:"Popular restaurant types near me",
+        list:[
+            "Bakeries near me",
+            "Bars near me",
+            "Beverage Shops near me",
+            "Bhojanalya near me",
+            "Cafés near me",
+            "Casual Dining near me",
+            "Dessert Parlors near me",
+            "Dhabas near me",
+            "Fine Dining near me",
+            "Food Courts near me",
+            "Food Trucks near me",
+            "Kiosks near me",
+            "Lounges near me",
+            "Meat Shops near me",
+            "Paan Shop near me",
+            "Pubs near me",
+            "Quick Bites near me",
+            "Sweet Shops near me"
+        ]
+    }
+]
   return (
     <div className='explore'>
         <div className='explore-content'>
             <p className='explore-heading'>Explore options near me</p>
 
-            <div className="cards-grid" > 
-                <Accordion >
-                    <Accordion.Item >
-                        <Accordion.Header className='accordion-header'>Popular cuisines near me</Accordion.Header>
-                        <Accordion.Body>
-                        BBQ food near meBakery food near meBeverages food near meBiryani food near meBurger food near meChettinad food near meChinese food near meDesserts food near meIce Cream food near meJuices food near meLebanese food near meMughlai food near meNorth Indian food near mePizza food near meSandwich food near meSeafood food near meShake food near meSichuan food near meSouth Indian food near meStreet food near me
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    </Accordion>
-            </div>
+            {
+                explores.map((item,index)=>{
+                    return(
+
+                        <div className="cards-grid" > 
+                            <Accordion style={{ marginBottom:"1.2rem"}}>
+                                <Accordion.Item >
+                                    <Accordion.Header className='accordion-header'>{item.name}</Accordion.Header>
+                                    <Accordion.Body className='accordion-content'>
+                                    <ul className="bullet-list">
+                                        {item.list.map((listItem, listIndex) => (
+                                        <li key={listIndex} className="bullet-list-item">
+                                            <span className="bullet">&#8226;</span>
+                                                {listItem}</li>
+                                        ))}
+                                    </ul>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                </Accordion>
+                        </div>
+                    )
+                })
+            }
             
 
         </div>
