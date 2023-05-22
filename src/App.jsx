dd8a06d673d186e860f47f3057c60f1be37d3348
Rithika <19cs076rithikak@ksrce.ac.in>
2023-05-22 11:00:55 +0530

    Routes  updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import IndexPage from './Pages/IndexPage/IndexPage'
 import LocationPage from './Pages/LocationPage/LocationPage'
+import {Routes,Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-
-      <LocationPage />
-      <IndexPage />
+      <Routes>
+        <Route path='/' element={<IndexPage />}></Route>
+        <Route path='coimbatore' element={<LocationPage />}></Route>
+      </Routes>
       </div>
   )
 }
