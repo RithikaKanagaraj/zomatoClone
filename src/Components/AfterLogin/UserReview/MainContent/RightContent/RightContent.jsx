8b3c3bcc6825a5cdbc1a4ec843ad257742742661
Rithika <19cs076rithikak@ksrce.ac.in>
2023-05-23 23:19:10 +0530

    review  updated

--- src/Components/AfterLogin/UserReview/MainContent/RightContent/RightContent.jsx
+++ src/Components/AfterLogin/UserReview/MainContent/RightContent/RightContent.jsx
@@ -4,7 +4,14 @@ import './RightContent.css'
 function RightContent() {
   return (
     <div className='review-right-div'>
-dghjkm
+      <section>
+        <h1 className='review-right-heading'>Reviews</h1>
+        <div className='review-right-main'>
+          <img className="review-right-image-div" src='https://res.cloudinary.com/dhtamjbrq/image/upload/v1684863901/Zomato/download_vqh7su.avif'></img>
+          <p className='review-right-image-content'>Nothing here yet</p>
+        </div>
+      </section>
+
     </div>
   )
 }
