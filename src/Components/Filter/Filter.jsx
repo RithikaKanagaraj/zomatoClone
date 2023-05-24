11d4b247043586f2866f529d33f55886cb0aed25
Rithika <19cs076rithikak@ksrce.ac.in>
2023-05-24 10:43:10 +0530

    filters  updated

--- src/Components/Filter/Filter.jsx
+++ src/Components/Filter/Filter.jsx
@@ -6,24 +6,11 @@ function Filter({svg,type}) {
 
   return (
     <div className='filterType'>
-       
-
-        
-        {
-            type === "Filters" && <div className='separate'>{svg} {type}</div> 
-        }
-        {
-            type==="Rating: 4.0+"  && <div className='separate'> {type}</div>
-        } 
-        {
-             type==="Pure Veg" && <div className='separate'>{type}</div>
-        }
-        {
-            type==="Cuisines" && <div className='separate'>{type} {svg}</div>
-        }
-        {
-            type==="More filters"&& <div className='separate'>{type} {svg}</div>
-        }
+       {svg && type ? (
+        <div className='separate'>{type} {svg} </div>
+      ) : (
+        <div className='separate'>{type}</div>
+      )}
    </div>
     
   )
